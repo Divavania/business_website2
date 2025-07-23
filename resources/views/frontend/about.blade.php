96cf75f6b9f0ce03c22d04bff8468e6116113fdf
LAPTOP-MC1VLL5R\ASUS <divavaniacandrawati@gmail.com>
2025-07-23 19:14:16 +0700

    update ui frontend about & contact

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.frontend')
 
-@section('title', 'Tentang Kami | Tigatra Adikara') {{-- Menambahkan title spesifik halaman --}}
-@section('description', 'Pelajari lebih lanjut tentang sejarah, visi, dan misi Tigatra Adikara.') {{-- Menambahkan deskripsi spesifik halaman --}}
-@section('keywords', 'Tigatra Adikara, tentang kami, sejarah, visi, misi, perusahaan') {{-- Menambahkan keywords spesifik halaman --}}
+@section('title', 'Tentang Kami | Tigatra Adikara')
+@section('description', 'Pelajari lebih lanjut tentang sejarah, visi, dan misi Tigatra Adikara.')
+@section('keywords', 'Tigatra Adikara, tentang kami, sejarah, visi, misi, perusahaan')
 
 @section('content')
 
@@ -11,7 +11,7 @@
             <h1 class="mb-2 mb-lg-0">Tentang Kami</h1>
             <nav class="breadcrumbs">
                 <ol>
-                    <li><a href="{{ url('/') }}">Home</a></li> {{-- Ubah link HTML menjadi Laravel URL/route --}}
+                    <li><a href="{{ url('/') }}">Home</a></li>
                     <li class="current">Tentang Kami</li>
                 </ol>
             </nav>
@@ -19,11 +19,8 @@
     </div>
 
     <section id="about-2" class="about-2 section">
-
         <div class="container" data-aos="fade-up">
-
             <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
-
                 <div class="col-lg-5">
                     <div class="about-img">
                         <img src="{{ asset('template-assets/assets/img/about-portrait.jpg') }}" class="img-fluid" alt="About Us Portrait"> 
@@ -31,7 +28,6 @@
                 </div>
 
                 <div class="col-lg-7">
-                    {{-- Judul utama bisa disesuaikan atau dihapus jika tidak diperlukan --}}
                     <h3 class="pt-0 pt-lg-5">Mengenal Lebih Dekat Tigatra Adikara</h3>
 
                     <ul class="nav nav-pills mb-3">
@@ -42,213 +38,55 @@
 
                     <!-- Tab Content -->
                     <div class="tab-content">
-
                         <div class="tab-pane fade show active" id="about-2-tab1">
-                            {{-- Konten Sejarah --}}
                             <h4 class="mt-4">Sejarah Perusahaan Kami</h4>
                             <p>{!! nl2br(e($about->sejarah)) !!}</p>
                         </div>
 
                         <div class="tab-pane fade" id="about-2-tab2">
-                            {{-- Konten Visi --}}
                             <h4 class="mt-4">Visi Kami</h4>
                             <p>{!! nl2br(e($about->visi)) !!}</p> 
                         </div>
 
                         <div class="tab-pane fade" id="about-2-tab3">
-                            {{-- Konten Misi --}}
                             <h4 class="mt-4">Misi Kami</h4>
                             <p>{!! nl2br(e($about->misi)) !!}</p> 
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
     </section>
 
     <section id="stats" class="stats section light-background">
-
         <div class="container" data-aos="fade-up" data-aos-delay="100">
-
             <div class="row gy-4">
-
                 <div class="col-lg-3 col-md-6">
                     <div class="stats-item text-center w-100 h-100">
                         <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
                         <p>Clients</p>
                     </div>
                 </div>
-
                 <div class="col-lg-3 col-md-6">
                     <div class="stats-item text-center w-100 h-100">
                         <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
                         <p>Projects</p>
                     </div>
                 </div>
-
                 <div class="col-lg-3 col-md-6">
                     <div class="stats-item text-center w-100 h-100">
                         <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1" class="purecounter"></span>
                         <p>Hours Of Support</p>
                     </div>
                 </div>
-
                 <div class="col-lg-3 col-md-6">
                     <div class="stats-item text-center w-100 h-100">
                         <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1" class="purecounter"></span>
                         <p>Workers</p>
                     </div>
                 </div>
-
             </div>
-
         </div>
+    </section>
 
-    </section><!-- /Stats Section -->
-
-    <!-- Team Section -->
-    <section id="team" class="team section">
-
-        <div class="container">
-
-            <div class="row gy-4">
-
-                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="team-member d-flex align-items-start">
-                        <div class="pic"><img src="{{ asset('template-assets/img/team/team-1.jpg') }}" class="img-fluid" alt=""></div> {{-- Ubah path gambar --}}
-                        <div class="member-info">
-                            <h4>Walter White</h4>
-                            <span>Chief Executive Officer</span>
-                            <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter-x"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""> <i class="bi bi-linkedin"></i> </a>
-                            </div>
-                        </div>
-                    </div>
-                </div><div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
-                    <div class="team-member d-flex align-items-start">
-                        <div class="pic"><img src="{{ asset('template-assets/img/team/team-2.jpg') }}" class="img-fluid" alt=""></div> {{-- Ubah path gambar --}}
-                        <div class="member-info">
-                            <h4>Sarah Jhonson</h4>
-                            <span>Product Manager</span>
-                            <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter-x"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""> <i class="bi bi-linkedin"></i> </a>
-                            </div>
-                        </div>
-                    </div>
-                </div><div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
-                    <div class="team-member d-flex align-items-start">
-                        <div class="pic"><img src="{{ asset('template-assets/img/team/team-3.jpg') }}" class="img-fluid" alt=""></div> {{-- Ubah path gambar --}}
-                        <div class="member-info">
-                            <h4>William Anderson</h4>
-                            <span>CTO</span>
-                            <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter-x"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""> <i class="bi bi-linkedin"></i> </a>
-                            </div>
-                        </div>
-                    </div>
-                </div><div class="col-lg-6" data-aos="fade-up" data-aos-delay="400">
-                    <div class="team-member d-flex align-items-start">
-                        <div class="pic"><img src="{{ asset('template-assets/img/team/team-4.jpg') }}" class="img-fluid" alt=""></div> {{-- Ubah path gambar --}}
-                        <div class="member-info">
-                            <h4>Amanda Jepson</h4>
-                            <span>Accountant</span>
-                            <p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter-x"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""> <i class="bi bi-linkedin"></i> </a>
-                            </div>
-                        </div>
-                    </div>
-                </div></div>
-
-        </div>
-
-    </section><!-- /Team Section -->
-
-    <!-- Skills Section -->
-    <section id="skills" class="skills section">
-
-        <!-- Section Title -->
-        <div class="container section-title" data-aos="fade-up">
-            <h2>Skills</h2>
-            <p>Check Our Skills<br></p>
-        </div><!-- End Section Title -->
-
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
-
-            <div class="row skills-content skills-animation">
-
-                <div class="col-lg-6">
-
-                    <div class="progress">
-                        <span class="skill"><span>HTML</span> <i class="val">100%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div><!-- End Skills Item -->
-
-                    <div class="progress">
-                        <span class="skill"><span>CSS</span> <i class="val">90%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div><!-- End Skills Item -->
-
-                    <div class="progress">
-                        <span class="skill"><span>JavaScript</span> <i class="val">75%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div><!-- End Skills Item -->
-
-                </div>
-
-                <div class="col-lg-6">
-
-                    <div class="progress">
-                        <span class="skill"><span>PHP</span> <i class="val">80%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div><!-- End Skills Item -->
-
-                    <div class="progress">
-                        <span class="skill"><span>WordPress/CMS</span> <i class="val">90%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div><!-- End Skills Item -->
-
-                    <div class="progress">
-                        <span class="skill"><span>Photoshop</span> <i class="val">55%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div><!-- End Skills Item -->
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </section>@endsection <!-- /Skills Section -->
+@endsection
--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.frontend') {{-- Sesuaikan dengan layout-mu --}}
+@extends('layouts.frontend')
 @section('title', 'Kontak')
 
 @section('content')
-<section id="contact" class="contact section"> {{-- Tambahkan kelas 'section' agar konsisten --}}
+<section id="contact" class="contact section">
     <div class="container" data-aos="fade-up">
         <div class="section-title">
             <h2>Kontak</h2>
@@ -17,13 +17,11 @@
                         <h4>Lokasi:</h4>
                         <p>Jalan Contoh No.123, Jakarta</p>
                     </div>
-
                     <div class="email mt-4">
                         <i class="bi bi-envelope"></i>
                         <h4>Email:</h4>
                         <p>info@contoh.com</p>
                     </div>
-
                     <div class="phone mt-4">
                         <i class="bi bi-phone"></i>
                         <h4>Telepon:</h4>
@@ -32,23 +30,24 @@
                 </div>
             </div>
 
-            <div class="col-lg-6"> {{-- Hapus mt-5 mt-lg-0 karena card akan memberi padding --}}
-                <div class="card shadow border rounded-lg p-4"> {{-- Mengubah shadow-sm border-0 menjadi shadow border --}}
-                    {{-- Menampilkan pesan sukses --}}
+            <div class="col-lg-6">
+                <div class="card shadow border rounded-lg p-4">
+                    {{-- Pesan sukses --}}
                     @if(session('success'))
-                        <div class="alert alert-success alert-dismissible fade show rounded-lg shadow-sm mb-3" role="alert">
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
                             <i class="bi bi-check-circle me-2"></i>
                             {{ session('success') }}
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>
                     @endif
-                    {{-- Menampilkan pesan error validasi --}}
+
+                    {{-- Pesan error --}}
                     @if ($errors->any())
-                        <div class="alert alert-danger alert-dismissible fade show rounded-lg shadow-sm mb-3" role="alert">
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
                             <i class="bi bi-exclamation-triangle me-2"></i>
                             Mohon periksa kembali input Anda:
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                            <ul class="mb-0">
+                            <ul class="mb-0 mt-2">
                                 @foreach ($errors->all() as $error)
                                     <li>{{ $error }}</li>
                                 @endforeach
@@ -56,57 +55,53 @@
                         </div>
                     @endif
 
-                    <form action="{{ route('contact.store') }}" method="POST" class="php-email-form"> {{-- Tambahkan method POST dan class php-email-form --}}
+                    {{-- FORM KONTAK --}}
+                    <form action="{{ route('contact.store') }}" method="POST">
                         @csrf
 
-                        <div class="row gy-4"> {{-- Gunakan gy-4 untuk gap vertikal --}}
+                        <div class="row gy-4">
                             <div class="col-md-6">
-                                <div class="form-group">
-                                    <input type="text" name="nama" class="form-control @error('nama') is-invalid @enderror" id="nama" placeholder="Nama Anda" value="{{ old('nama') }}" required> {{-- Menghapus rounded-pill --}}
-                                    @error('nama')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
+                                <input type="text" name="nama" class="form-control @error('nama') is-invalid @enderror" placeholder="Nama Anda" value="{{ old('nama') }}" required>
+                                @error('nama')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
-                            <div class="col-md-6"> {{-- Hapus mt-3 mt-md-0, gy-4 sudah menangani --}}
-                                <div class="form-group">
-                                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="Email Anda" value="{{ old('email') }}" required> {{-- Menghapus rounded-pill --}}
-                                    @error('email')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
+                            <div class="col-md-6">
+                                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email Anda" value="{{ old('email') }}" required>
+                                @error('email')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
 
                         <div class="form-group mt-3">
-                            <input type="text" class="form-control @error('nomor_hp') is-invalid @enderror" name="nomor_hp" id="nomor_hp" placeholder="Nomor Telepon (misal: 081234567890)" value="{{ old('nomor_hp') }}" required> {{-- Menghapus rounded-pill --}}
+                            <input type="text" name="nomor_hp" class="form-control @error('nomor_hp') is-invalid @enderror" placeholder="Nomor Telepon" value="{{ old('nomor_hp') }}" required>
                             @error('nomor_hp')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
 
                         <div class="form-group mt-3">
-                            <input type="text" class="form-control @error('subjek') is-invalid @enderror" name="subjek" id="subjek" placeholder="Subjek" value="{{ old('subjek') }}" required> {{-- Menghapus rounded-pill --}}
+                            <input type="text" name="subjek" class="form-control @error('subjek') is-invalid @enderror" placeholder="Subjek" value="{{ old('subjek') }}" required>
                             @error('subjek')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
 
                         <div class="form-group mt-3">
-                            <textarea class="form-control @error('pesan') is-invalid @enderror" name="pesan" rows="6" placeholder="Tulis pesan Anda di sini..." required>{{ old('pesan') }}</textarea> {{-- Menghapus rounded-lg --}}
+                            <textarea name="pesan" rows="6" class="form-control @error('pesan') is-invalid @enderror" placeholder="Tulis pesan Anda di sini..." required>{{ old('pesan') }}</textarea>
                             @error('pesan')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
 
-                        <div class="col-md-12 text-center mt-3"> {{-- Tambahkan col-md-12 dan text-center --}}
-                            <div class="loading">Loading</div>
-                            <div class="error-message"></div>
-                            <div class="sent-message">Pesan Anda berhasil terkirim. Terima kasih!</div>
-                            <button type="submit" class="btn btn-primary rounded-pill px-4 py-2">Kirim Pesan</button>
+                        <div class="text-center mt-4">
+                            <button type="submit" class="btn btn-primary px-4 py-2 rounded-pill">
+                                Kirim Pesan
+                            </button>
                         </div>
                     </form>
-                </div> {{-- Tutup wrapper card --}}
+                </div>
             </div>
         </div>
     </div>
@@ -115,26 +110,19 @@
 
 @push('styles')
 <style>
-    /* Custom styles for form controls */
     .form-control {
-        border: 1px solid #ced4da; /* Default Bootstrap border color */
+        border: 1px solid #ced4da;
         transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
     }
 
     .form-control:focus {
-        border-color: #86b7fe; /* Bootstrap primary color for focus */
-        box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25); /* Bootstrap primary shadow for focus */
+        border-color: #86b7fe;
+        box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
     }
 
-    /* Placeholder styling */
     .form-control::placeholder {
-        color: #6c757d; /* Darker grey for better visibility */
-        opacity: 0.7; /* Slightly less opaque than default for elegance */
+        color: #6c757d;
+        opacity: 0.7;
     }
 </style>
-@endpush
-
-@push('scripts')
-{{-- Jika Anda menggunakan php-email-form/validate.js, pastikan itu dimuat di layouts/frontend.blade.php --}}
-{{-- Tidak perlu script tambahan di sini karena php-email-form/validate.js sudah menangani --}}
 @endpush
